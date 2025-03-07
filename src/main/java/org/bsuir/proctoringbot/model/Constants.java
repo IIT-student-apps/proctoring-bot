87f86efe223304ab92ecb0eeaed3d691fa9730f7
MaxMart71 <114000605+MaxMart71@users.noreply.github.com>
2025-03-07 21:56:00 +0300

    add TeacherMenuController and PICK_TEACHER_MENU_ITEM state

---
+++ src/main/java/org/bsuir/proctoringbot/model/Constants.java
@@ -0,0 +1,15 @@
+package org.bsuir.proctoringbot.model;
+
+public class Constants {
+
+    //меню преподавателя
+    public static final String UPDATE_SUBJECT_INFO_BUTTON_MESSAGE = "Обновить информацию о предмете";
+    public static final String EDIT_LAB_WORK_BUTTON_MESSAGE = "Корректировка лабораторной работы";
+    public static final String WORK_WITH_TESTS_BUTTON_MESSAGE = "Работа с тестами";
+
+    public static final String UPDATE_SUBJECT_INFO_BUTTON_CALLBACK = "Обновить информацию о предмете";
+    public static final String EDIT_LAB_WORK_BUTTON_CALLBACK = "Корректировка лабораторной работы";
+    public static final String WORK_WITH_TESTS_BUTTON_CALLBACK = "Работа с тестами";
+
+
+}
