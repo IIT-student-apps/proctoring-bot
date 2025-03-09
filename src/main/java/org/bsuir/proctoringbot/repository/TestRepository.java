1790719b86b161456400c0985c1ae2f716aa3892
Semenov Egor <113463729+egorsemenovv@users.noreply.github.com>
2025-03-09 20:37:16 +0300

    ref: remove unused imports

--- src/main/java/org/bsuir/proctoringbot/repository/TestRepository.java
+++ src/main/java/org/bsuir/proctoringbot/repository/TestRepository.java
@@ -1,7 +1,6 @@
 package org.bsuir.proctoringbot.repository;
 
 import org.bsuir.proctoringbot.model.Test;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
